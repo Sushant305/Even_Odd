fc29af20c93d898672be41469c192a50f5d234e3
Sushant305 <sushantn2005@gmail.com>
2026-06-30 14:05:32 +0530

    Add Even_Odd number checker project
    Initialize project with HTML structure, JavaScript implementation using if-else method to check even and odd numbers, project screenshot for favicon, and updated README with project description.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Even_Odd
+
+Here i use the if else method for both even and odd numbers and if you enter the character then else part will execute 
---
+++ script.js
@@ -0,0 +1,14 @@
+let EvenNumber = 10;
+let OddNumber = 5;
+
+if (EvenNumber%2==0){
+    console.log("The Number",EvenNumber,"is Even");
+}else{
+    console.log("you Enter the character");
+}
+
+if(OddNumber%2 != 0){
+    console.log("The Number",OddNumber,"is Odd");
+}else{
+    console.log("you Enter the Character ");
+}
